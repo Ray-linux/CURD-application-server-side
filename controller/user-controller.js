c32bf2825b98462e8c45ba4615444545e1b7b851
Ray-linux <ranirava52@gmail.com>
2023-01-28 14:00:04 +0530

    update & deleting

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -24,3 +24,33 @@ export const getUsers = async(req, res) => {
         res.status(404).json({message: e.message})
     }
 }
+
+export const getUser = async (req, res) => {
+    try{
+        const user = await User.find({_id : req.params.id});
+        res.status(200).json(user);
+    }catch(e){
+        res.status(404).json({message: e.message})
+    }
+}
+
+export const editUser = async (req, res) => {
+    const user = req.body;
+    const editUser = new User(user);
+
+    try{
+        await User.updateOne({_id: req.params.id}, editUser);
+        res.status(201).json(editUser)
+    }catch(e){
+        res.status(409).json({message: e.message})
+    }
+}
+
+export const deleteUser = async(req, res) => {
+    try {
+        await User.deleteOne({_id: req.params.id});
+        res.status(200).json({message: 'User deleted successfully'})
+    } catch (e) {
+        res.status(409).json({message: e.message})
+    }
+}
